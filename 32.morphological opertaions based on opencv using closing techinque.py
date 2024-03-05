f7e5e11445c17c32e32ce45b6034d38a16477877
Adi charan <122437019+Adicharan948@users.noreply.github.com>
2024-03-05 17:40:00 +0530

    Add files via upload

---
+++ 32.morphological opertaions based on opencv using closing techinque.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+img=cv2.imread("C:/Users/adich/Downloads/1.jpg")
+kernel=np.ones((5,5),np.uint8)
+closing=cv2.morphologyEx(img,cv2.MORPH_CLOSE,kernel)
+cv2.imshow("original",img)
+cv2.imshow("closing",closing)
+cv2.waitKey(0)
+cv2.destoryAllWindows()
---
+++ 33.morphological opeations based on opencv using gradient techinque.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+img=cv2.imread("C:/Users/adich/Downloads/1.jpg",cv2.IMREAD_GRAYSCALE)
+kernel=np.ones((5,5),np.uint8)
+grad=cv2.morphologyEx(img,cv2.MORPH_GRADIENT,kernel)
+cv2.imshow("original",img)
+cv2.imshow("Gradent",grad)
+cv2.waitKey(0)
+cv2.destoryAllWindows()
